fb2c2065c47ff043ef363bf495b36763dda4cbf3
ng <NGalygich@yandex.ru>
2023-04-24 16:30:10 +1000

    error conected with db

--- Context.cs
+++ Context.cs
@@ -7,7 +7,6 @@ public class ApplicationContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //optionsBuilder.UseSqlServer("Data Source=192.168.0.116;Database=test_galyga;user id=sa;password=compasdt;Integrated Security=false;Trusted_Connection=False;Persist Security Info=True;MultipleActiveResultSets=true");
-       
+        optionsBuilder.UseSqlServer("Data Source=192.168.0.116;Database=test_galyga;user id=sa;password=compasdt;Integrated Security=false;Trusted_Connection=False;Persist Security Info=True;MultipleActiveResultSets=true");
     }
 }
