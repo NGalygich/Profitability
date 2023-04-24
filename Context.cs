4b544bc8012e5acbeccdcc26cda6735cdbbdc40f
ng <NGalygich@yandex.ru>
2023-04-24 17:43:17 +1000

    hz

--- Context.cs
+++ Context.cs
@@ -1,12 +1,48 @@
 using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
  
 public class ApplicationContext : DbContext
 {
+    
     public DbSet<User> Users => Set<User>();
     public ApplicationContext() => Database.EnsureCreated();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Data Source=192.168.0.116;Database=test_galyga;user id=sa;password=compasdt;Integrated Security=false;Trusted_Connection=False;Persist Security Info=True;MultipleActiveResultSets=true");
+        // var builder = new SqlConnectionStringBuilder
+        // {
+        //     DataSource = @"IP_ADDRESS\INSTANCE",
+        //     InitialCatalog = "DbName",
+        //     UserID = "MyUserId",
+        //     Password = "MyPassword"
+        // };
+
+        // var connectionString = builder.ConnectionString;
+
+        // // Use connection string
+        // optionsBuilder.UseSqlServer(connectionString );
+
+        optionsBuilder.UseSqlServer("server=192.168.0.116;database=test_galyga;user id=sa;password=compasdt;Integrated Security=false;Trusted_Connection=False;Persist Security Info=True;MultipleActiveResultSets=true");
     }
+    
+    /*
+    protected readonly IConfiguration Configuration;
+    public DataContext() => Database.EnsureCreated();
+
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sql server with connection string from app settings
+        options.UseSqlServer(Configuration.GetConnectionString("server=192.168.0.116;database=test_galyga;user id=sa;password=compasdt;Integrated Security=false;Trusted_Connection=False;Persist Security Info=True;MultipleActiveResultSets=true"));
+    }
+
+    public DbSet<User> Users { get; set; }
+    */
 }
