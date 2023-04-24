2ee84015a2f287280b7291c396ff28098da0edfa
ng <NGalygich@yandex.ru>
2023-04-24 16:25:02 +1000

    error conected with db

---
+++ Context.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+ 
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //optionsBuilder.UseSqlServer("Data Source=192.168.0.116;Database=test_galyga;user id=sa;password=compasdt;Integrated Security=false;Trusted_Connection=False;Persist Security Info=True;MultipleActiveResultSets=true");
+       
+    }
+}
