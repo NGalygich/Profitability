c1a9d937002a9539a6dbee03d26a69f0bed1be57
ng <NGalygich@yandex.ru>
2023-04-28 09:58:38 +1000

    add key id in db

--- Data/PaymentClientsPlanned.cs
+++ Data/PaymentClientsPlanned.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Profitability;
 
 public partial class PaymentClientsPlanned
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public DateTime? DatePlanned { get; set; }
--- Data/WorkingDaysMercenary.cs
+++ Data/WorkingDaysMercenary.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Profitability;
 
 public partial class WorkingDaysMercenary
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public DateTime? WorkingDate { get; set; }
