d8980032a68f39eac8db0af1c0184324c220a016
ng <NGalygich@yandex.ru>
2023-04-28 15:38:53 +1000

    .

--- Models/ReportGeneration.cs
+++ Models/ReportGeneration.cs
@@ -1,99 +1,39 @@
 using System;
-using System.IO;
 using System.Windows;
-using System.Drawing;
-using Excel = Microsoft.Office.Interop.Excel;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _excel = Microsoft.Office.Interop.Excel;
+
 
 public class RepoportGeneration
 {
+     _Application excelapp = new _excel.Application();
+    Workbook wb;
+    Worksheet ws;
+    public void CreateFile() {
+        this.wb = excelapp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+        this.ws = this.wb.Worksheets[1];
+    }
+    public void CreateSheet() {
+        Worksheet worksheet1 = excelapp.Worksheets.Add(After: this.ws);
+    }
+
+    public void SaveAs(string filepath) {
+        wb.SaveAs(filepath);
+    }
     public static void Generation()
     {
+        
         try
         {
-            //using(new ExcelUILanguageHelper())
-            //{
-                string fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + "ExcelReport.xlsx";
-
-                Excel.Application xlsApp;
-                Excel.Workbook xlsWorkbook;
-                Excel.Worksheet xlsWorksheet;
-                object misValue = System.Reflection.Missing.Value;
-
-                // Remove the old excel report file
-                try
-                {
-                    FileInfo oldFile = new FileInfo(fileName);
-                    if(oldFile.Exists)
-                    {
-                        File.SetAttributes(oldFile.FullName, FileAttributes.Normal);
-                        oldFile.Delete();
-                    }
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message,"Ошибка",MessageBoxButton.OK,MessageBoxImage.Error);
-                    return;
-                }
-
-                try
-                {
-                    xlsApp = new Excel.Application();
-                    xlsWorkbook = xlsApp.Workbooks.Add(misValue);
-                    xlsWorksheet = (Excel.Worksheet)xlsWorkbook.Sheets[1];
-
-                    // Create the header for Excel file
-                    xlsWorksheet.Cells[1, 1] = "Example of Excel report. Get data from pubs database, table authors";
-                    Excel.Range range = xlsWorksheet.get_Range("A1", "E1");
-                    range.Merge(1);
-                    range.Borders.Color = Color.Black.ToArgb();
-                    range.Interior.Color = Color.Yellow.ToArgb();
-
-                    int i = 3;
-
-                    //using SqlConnection conn = new SqlConnection(connectionString);
-                    //conn.Open();
-                    //using SqlCommand cmd = new SqlCommand(sqlselect, conn);
-                    //using SqlDataReader dr = cmd.ExecuteReader();
-                    // if(dr.HasRows)
-                    // {
-                    //     for(int j = 0; j < dr.FieldCount; ++j)
-                    //     {
-                    //         xlsWorksheet.Cells[i, j + 1] = dr.GetName(j);
-                    //     }
-                    //     ++i;
-                    // }
-
-                    // while(dr.Read())
-                    // {
-                    //     for(int j = 1; j <= dr.FieldCount; ++j)
-                    //         xlsWorksheet.Cells[i, j] = dr.GetValue(j - 1);
-                    //     ++i;
-                    // }
-
-                    range = xlsWorksheet.get_Range("A2", "I" + (i + 2).ToString());
-                    range.Columns.AutoFit();
-
-                    xlsWorkbook.SaveAs(fileName, Excel.XlFileFormat.xlWorkbookDefault, misValue, misValue, misValue, misValue,
-                        Excel.XlSaveAsAccessMode.xlExclusive, Excel.XlSaveConflictResolution.xlLocalSessionChanges, misValue, misValue, misValue, misValue);
-                    xlsWorkbook.Close(true, misValue, misValue);
-                    xlsApp.Quit();
-
-                    // ReleaseObject(xlsWorksheet);
-                    // ReleaseObject(xlsWorkbook);
-                    // ReleaseObject(xlsApp);
-
-                    // if(MessageBox.Show("Excel report has been created on your desktop\nWould you like to open it?", "Created Excel report",
-                    //     MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
-                    // {
-                    //     Process.Start(fileName);
-                    // }
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.Message,"Ошибка",MessageBoxButton.OK,MessageBoxImage.Error);
-                }
-            //}
+            Excel Excelfile = new Excel();
+            Excelfile.CreateFile();
+            Excelfile.CreateSheet();
+            Excelfile.SaveAs(@"C:/Users/galyga_na/Desktop/ExcelfilebyZeeshan.xlsx");
+            Excelfile.wb.Close();
             MessageBox.Show("Объект успешно сохранен");
 
         }
